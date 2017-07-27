ba257bbabc852b32ba114e695e6785b8997faa43
RalucaEne <raluca.ene8@yahoo.ro>
2017-07-27 21:56:15 +0300

    First

--- chirp/urls.py
+++ chirp/urls.py
@@ -7,6 +7,10 @@ from django.contrib.auth import views as auth_views
 from message.views import RegisterView, TimelineView, MyProfileView, ProfileView, follow_user, new_chirp, unfollow_user, \
     like_message
 
+
+
+
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', TimelineView.as_view(), name='index'),
@@ -19,6 +23,7 @@ urlpatterns = [
     url(r'^unfollow/(?P<username>[-\w]+)/$', login_required(unfollow_user), name='unfollow_user'),
     url(r'^chirp/', login_required(new_chirp),name='chirp'),
     url(r'^like/$',login_required(like_message),name='like')
+
 
 
 ]
