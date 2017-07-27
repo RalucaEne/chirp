e37db2a2e76964e29dc6e0c8a4ac383cf1779bb1
RalucaEne <raluca.ene8@yahoo.ro>
2017-07-27 12:28:10 +0300

    First

--- message/models.py
+++ message/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
@@ -11,13 +12,31 @@ class Message(TimestampModel):
     user = models.ForeignKey(User)
     status = models.TextField(null=False, max_length=140, blank=False)
 
+    def __str__(self):
+        return self.status
+    @property
+    def like_users(self):
+        return[l.user for l in self.like_set.all() if l.like]
+
+    @property
+    def dislike_users(self):
+        return[l.user for l in self.like_set.all() if not l.like]
 
 class Like(TimestampModel):
+    objects = None
     user = models.ForeignKey(User)
     message =models.ForeignKey(Message)
     like = models.BooleanField()
 
 
+
 class Follow(TimestampModel):
+    object = None
+
+    class Meta:
+        unique_together = [("followed_user", "following_user"),]
     followed_user = models.ForeignKey(User, related_name='followed_by')
     following_user = models.ForeignKey(User, related_name='following')
+
+    def __str__(self):
+        return "User {0} follows user {1}".format(self.following_user.username, self.followed_user.username)
